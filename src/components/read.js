9ab4abdf4959b40d5985fe68e9942d6bec37229a
Blaithnaid <iarlaken@gmail.com>
2024-10-14 14:46:00 +0100

    adding axios

--- src/components/read.js
+++ src/components/read.js
@@ -1,33 +1,20 @@
+import { useEffect, useState } from "react";
 import Movies from "./movies.js";
+import axios from "axios";
 
 const Read = () => {
-  const movies = [
-    // const containing all the data for our movies
-    {
-      Title: "Avengers: Infinity War",
-      Year: "2018",
-      imdbID: "tt4154756",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Captain America: Civil War",
-      Year: "2016",
-      imdbID: "tt3498820",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg",
-    },
-    {
-      Title: "World War Z",
-      Year: "2013",
-      imdbID: "tt0816711",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg",
-    },
-  ];
+  const data = [];
+  const [movies, setMovies] = useState(data);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonblob.com/api/jsonblob/1287718524221775872")
+      .then((response) => {
+        console.log(response.data.movies);
+        setMovies(response.data.movies);
+      })
+      .catch(() => {});
+  });
   return (
     // return the Read component, containing the Movies component, into which we pass the movies data
     <div>
