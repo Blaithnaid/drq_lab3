5f5720969509b6f1e0b75f810c33663819e8b185
Blaithnaid <iarlaken@gmail.com>
2024-11-04 14:39:26 +0000

    can now submit films to local server

--- src/components/create.js
+++ src/components/create.js
@@ -1,54 +1,66 @@
 import { useState } from "react";
 
 const Create = () => {
-  const [title, setTitle] = useState("");
-  const [year, setYear] = useState("");
-  const [poster, setPoster] = useState("");
+	const [title, setTitle] = useState("");
+	const [year, setYear] = useState("");
+	const [poster, setPoster] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted!");
-  };
-  return (
-    <div>
-      <h3>Hello from the Create component!</h3>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Add Movie Title: </label>
-          <input
-            type="text"
-            className="form-control"
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-        </div>
-        <div className="form-group">
-          <label>Add Movie Year: </label>
-          <input
-            type="text"
-            className="form-control"
-            value={year}
-            onChange={(e) => {
-              setYear(e.target.value);
-            }}
-          />
-        </div>
-        <div className="form-group">
-          <label>Add Movie Poster: </label>
-          <textarea
-            type="text"
-            className="form-control"
-            value={poster}
-            onChange={(e) => {
-              setPoster(e.target.value);
-            }}
-          />
-        </div>
-      </form>
-    </div>
-  );
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// log details from the form
+		console.log(`Title: ${title}, Year: ${year}, Poster: ${poster}`);
+		// create a new movie object
+		const movie = {
+			title: title,
+			year: year,
+			poster: poster,
+		};
+
+		axios
+			.post("http://localhost:4000/api/movies", movie)
+			.then((res) => console.log(res.data))
+			.catch((err) => console.log(err.data));
+	};
+	return (
+		<div>
+			<h3>Hello from the Create component!</h3>
+			<form onSubmit={handleSubmit}>
+				<div className="form-group">
+					<label>Add Movie Title: </label>
+					<input
+						type="text"
+						className="form-control"
+						value={title}
+						onChange={(e) => {
+							setTitle(e.target.value);
+						}}
+					/>
+				</div>
+				<div className="form-group">
+					<label>Add Movie Year: </label>
+					<input
+						type="text"
+						className="form-control"
+						value={year}
+						onChange={(e) => {
+							setYear(e.target.value);
+						}}
+					/>
+				</div>
+				<div className="form-group">
+					<label>Add Movie Poster: </label>
+					<textarea
+						type="text"
+						className="form-control"
+						value={poster}
+						onChange={(e) => {
+							setPoster(e.target.value);
+						}}
+					/>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default Create;
