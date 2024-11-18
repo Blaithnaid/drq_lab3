286cde37759c6252e9d0ca4639a45aeccb8f7b0e
Blaithnaid <iarlaken@gmail.com>
2024-11-18 14:32:18 +0000

    add edit route

--- src/App.js
+++ src/App.js
@@ -7,20 +7,22 @@ import Content from "./components/content";
 import Create from "./components/create";
 import Read from "./components/read";
 import Movies from "./components/movies";
+import Edit from "./components/edit";
 
 function App() {
-  return (
-    <Router>
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<Content />} />
-        <Route path="/read" element={<Read />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
+	return (
+		<Router>
+			<NavigationBar />
+			<Routes>
+				<Route path="/" element={<Content />} />
+				<Route path="/read" element={<Read />} />
+				<Route path="/create" element={<Create />} />
+				<Route path="/edit/:id" element={<Edit />} />
+			</Routes>
 
-      <Footer />
-    </Router>
-  );
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
